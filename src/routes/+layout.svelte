820ce5c639b333da6a6a226ee42b3f8d10ae66bc
polardune-dev <62209607+polardune-dev@users.noreply.github.com>
2023-07-23 22:51:20 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">Logo</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn" href="https://help.sample.com" target="_blank" rel="noreferrer">Help</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail>
+			<svelte:fragment>
+				<AppRailAnchor href="/"><svelte:fragment slot="lead"><i class="fa-solid fa-home" /></svelte:fragment></AppRailAnchor>
+				<AppRailAnchor href="/things"><svelte:fragment slot="lead"><i class="fa-solid fa-list" /></svelte:fragment></AppRailAnchor>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<AppRailAnchor href="/settings"
+					><svelte:fragment slot="lead"><i class="fa-solid fa-gear" /></svelte:fragment><span
+						>Settings</span
+					></AppRailAnchor
+				>
+				<AppRailAnchor href="/about">About</AppRailAnchor>
+			</svelte:fragment>
+		</AppRail>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ src/routes/things/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Table } from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
+	import { tableMapperValues } from '@skeletonlabs/skeleton';
+
+	// Sample stuff
+	const sourceData = [
+		{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+		{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+		{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+		{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+		{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' }
+	];
+
+	const tableSimple: TableSource = {
+		// A list of heading labels.
+		head: ['Name', 'Symbol', 'Weight'],
+		// The data visibly shown in your table body UI.
+		body: tableMapperValues(sourceData, ['name', 'symbol', 'weight']),
+		// Optional: The data returned when interactive is enabled and a row is clicked.
+		meta: tableMapperValues(sourceData, ['position', 'name', 'symbol', 'weight']),
+		// Optional: A list of footer labels.
+		foot: ['Total', '', '<code class="code">5</code>']
+	};
+</script>
+
+<div class="container">
+	<div class="space-y-5 space-x-5">
+		<h1 class="h1">Things</h1>
+		<Table source={tableSimple} />
+	</div>
+</div>
