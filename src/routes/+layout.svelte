114f94ca276cc32ce1302cbcf819bc26920de6cc
polardune-dev <62209607+polardune-dev@users.noreply.github.com>
2023-08-20 12:35:34 +0200

    change: highlight selected page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
 	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import { page } from '$app/stores';
 </script>
 
 <AppShell>
@@ -25,16 +26,16 @@
 	<svelte:fragment slot="sidebarLeft">
 		<AppRail>
 			<svelte:fragment>
-				<AppRailAnchor href="/"><svelte:fragment slot="lead"><i class="fa-solid fa-home" /></svelte:fragment></AppRailAnchor>
-				<AppRailAnchor href="/things"><svelte:fragment slot="lead"><i class="fa-solid fa-list" /></svelte:fragment></AppRailAnchor>
+				<AppRailAnchor href="/" selected={$page.url.pathname === '/'}><svelte:fragment slot="lead"><i class="fa-solid fa-home" /></svelte:fragment></AppRailAnchor>
+				<AppRailAnchor href="/things" selected={$page.url.pathname === '/things'}><svelte:fragment slot="lead"><i class="fa-solid fa-list" /></svelte:fragment></AppRailAnchor>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<AppRailAnchor href="/settings"
+				<AppRailAnchor href="/settings" selected={$page.url.pathname === '/settings'}
 					><svelte:fragment slot="lead"><i class="fa-solid fa-gear" /></svelte:fragment><span
 						>Settings</span
 					></AppRailAnchor
 				>
-				<AppRailAnchor href="/about">About</AppRailAnchor>
+				<AppRailAnchor href="/about" selected={$page.url.pathname === '/about'}>About</AppRailAnchor>
 			</svelte:fragment>
 		</AppRail>
 	</svelte:fragment>
